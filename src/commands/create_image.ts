a6a89269a33fc6bb028b0dc9836d30234778cbe9
Hostagen <68284806+Hostagen@users.noreply.github.com>
2022-01-14 15:53:53 +0900

    edit command script.

--- src/commands/create_image.ts
+++ src/commands/create_image.ts
@@ -1,16 +1,12 @@
 import { SlashCommandBuilder} from '@discordjs/builders';
-import { CommandInteraction } from 'discord.js';
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('말대꾸하기')
         .setDescription('○○가 말대꾸?')
         .addStringOption(option => 
-            option.setName('○○')
+            option.setName('이름')
                 .setDescription('무엇이 말대꾸?')
                 .setRequired(true)
-        ),
-    async execute(interaction: CommandInteraction) {
-        return interaction.reply('Pong!');
-    },
+        )
 };
